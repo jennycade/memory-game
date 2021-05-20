0ce7918616e590739eb1b2d87bd8519587386293
Jenny Zonka <jennycade@gmail.com>
2021-05-20 11:10:06 -0700

    More tweaking that gets me no where. About to abandon ship.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import Game from './Game';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [gameStatus, setGameStatus] = useState('playing');
+  const [game, setGame] = useState([]);
 
-  const loseGame = () => {
-    setGameStatus('lost');
-    setTimeout(() => setGameStatus('playing'), 5000); // sort of works? re-renders twice.
-    
+  const resetGame = () => {
+    setGame(<p>Reset game.</p>);
+    setGame(<Game numCards={3} loseGame={ resetGame } score={0} />); // doesn't start a new game
   }
+
+  useEffect(() => {
+    setGame(<Game numCards={3} loseGame={ resetGame } score={0} />);
+  }, []);
+
+  
   console.log('I am a line in App.js. I am being run.');
 
   // something in the render has to change in order to start a new game.
   // TODO: cards get new images but not new number of times they've been clicked—why?
   return (
     <div className="App">
-      <p>{gameStatus}</p> 
-      <Game numCards={3} loseGame={loseGame} gameStatus={gameStatus} />
+      { game }
     </div>
   );
 }
