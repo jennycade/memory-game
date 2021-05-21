c6c7b1a9b7cd26efebfef3f7452ad1ffef967795
Jenny Zonka <jennycade@gmail.com>
2021-05-21 13:30:46 -0700

    Disable clicking on Card until image is loaded

--- src/Card.js
+++ src/Card.js
@@ -4,29 +4,35 @@ import { useEffect, useState } from "react";
 
 
 const Card = (props) => {
+  const [clickEnabled, setClickEnabled] = useState(false);
 
   const [clicks, setClicks] = useState(0);
 
   const clickCard = () => {
-    if (clicks === 0) {
-      // up clicks
-      // score point
-      setClicks(clicks + 1);
-      props.scorePoint();
-      props.shuffleCards();
-    } else {
-      props.newGame(3);
+    if (clickEnabled) {
+      if (clicks === 0) {
+        // up clicks
+        // score point
+        setClicks(clicks + 1);
+        props.scorePoint();
+        props.shuffleCards();
+      } else {
+        props.newGame(3);
+      }
+      
     }
   }
 
   useEffect(() => {
     setClicks(0);
+    setClickEnabled(false);
   }, [props.gameNumber]);
 
   return (
-    <div style={{order: props.order}} className="Card" onClick={clickCard}>
+    <div style={{order: props.order}} className="Card" onLoad={() => {setClickEnabled(true)}} onClick={clickCard}>
       <img src={ props.photo } alt={ props.veggieName } />
       <p>{ props.veggieName }</p>
+      <p>{ clickEnabled ? 'click enabled' : 'click not enabled' }</p>
     </div>
   );
 }
